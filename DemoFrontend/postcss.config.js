e87ae7cab6abfafcebb413e8a33d06b48e5ceb92
Yashwanth <yashwanth567@gmail.com>
2026-03-27 14:25:34 +0530

    changes again file names tailwidn cinfig

--- DemoFrontend/postcss.config.js
+++ DemoFrontend/postcss.config.js
@@ -1,6 +1,11 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
   },
-}
+  plugins: [],
+};
--- DemoFrontend/tailwind.Config.js
+++ DemoFrontend/tailwind.Config.js
@@ -1,8 +1,9 @@
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-  "./index.html",
-  "./src/**/*.{js,jsx,ts,tsx}",  // add all types
-],
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
